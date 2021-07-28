aec653bccee2021a788f3fd1da7f4fe619e93380
MattWellDressed <mattm@lcpud.org>
2021-07-28 11:54:27 -0700

    Displaying Table Data

--- js/site.js
+++ js/site.js
@@ -1,11 +1,3 @@
-function GetFormValues() {
-    let principalAmount = document.getElementById("loanAmount").value;
-    let interestRate = document.getElementById("rate").value;
-    let loanLength = document.getElementById("loanTerm").value;
-    let paymentAmount = Number.parseFloat((principalAmount) * (interestRate / 1200) / (1 - (1 + interestRate / 1200)**(0 - loanLength))).toFixed(2);
-    return { principalAmount, interestRate, loanLength, paymentAmount};
-}
-
 function GenerateTableTemplate(loanLength) {
     const rowTemplate = "<tr><td>?</td><td>?</td><td>?</td><td>?</td><td>?</td><td>?</td></tr>"
     let tableTemplate = "";
@@ -16,21 +8,19 @@ function GenerateTableTemplate(loanLength) {
     return tableTemplate;
 }
 
+function InputDataIntoTableTemplate(loanLength ,loanObj) {
+    let theLoan = loanObj;
+    let tableTemplate = GenerateTableTemplate(loanLength);
 
+    for (let index = 1; index <= theLoan.duration; index++) {
 
-function InputDataIntoTableTemplate() {
-    let formValues = GetFormValues();
-    let tableTemplate = GenerateTableTemplate(formValues.loanLength);
-
-    for (let index = 1; index <= formValues.loanLength; index++) {
-
-        
+        let holder = theLoan.loanUpdate();
         tableTemplate = tableTemplate.replace("?", index.toString());
-        tableTemplate = tableTemplate.replace("?", formValues.paymentAmount.toString());
-        tableTemplate = tableTemplate.replace("?", );
-        tableTemplate = tableTemplate.replace("?", );
-        tableTemplate = tableTemplate.replace("?", );
-        tableTemplate = tableTemplate.replace("?", (formValues.principalAmount - (formValues.paymentAmount * index)).toString() );
+        tableTemplate = tableTemplate.replace("?", Number.parseFloat(theLoan.totalPayment).toFixed(2));
+        tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[2]).toFixed(2));
+        tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[3]).toFixed(2));
+        tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[4]).toFixed(2));
+        tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[5]).toFixed(2));
         
     }
 
@@ -40,5 +30,42 @@ function InputDataIntoTableTemplate() {
 
 
 function BtnClickEventFunction() {
-    document.getElementById("results").innerHTML = InputDataIntoTableTemplate();
+    const loanInfo = {
+        amount: parseFloat(document.getElementById("loanAmount").value),
+        balance: parseFloat(document.getElementById("loanAmount").value),
+        paymentsLeft: parseInt(document.getElementById("loanTerm").value),
+        interest: parseFloat(document.getElementById("rate").value),
+        duration: parseInt(document.getElementById("loanTerm").value),
+        get totalPayment(){
+            return (this.amount) * (this.interest / 1200) / (1 - (1 + this.interest / 1200)**(0 - this.duration));
+        },
+        interestPaid: 0,
+        interestPayment: 0,
+        principalPayment: 0,
+        loanUpdate: function(){
+            if (this.amount == this.balance) {
+                this.interestPayment = this.amount * (this.interest / 1200);
+                this.interestPaid += this.interestPayment;
+                this.principalPayment = this.totalPayment - this.interestPayment;
+                this.balance = this.amount - this.principalPayment;
+                //this.paymentsLeft--;
+                
+            }
+            else
+            {
+                this.interestPayment = this.balance * (this.interest / 1200);
+                this.interestPaid += this.interestPayment;
+                this.principalPayment = this.totalPayment - this.interestPayment;
+                this.balance = this.balance - this.principalPayment;
+                //this.paymentsLeft--;
+            }
+    
+            return [this.paymentsLeft--, this.totalPayment, this.principalPayment, this.interestPayment, this.interestPaid, this.balance]
+        }
+    };
+    
+    document.getElementById("results").innerHTML = InputDataIntoTableTemplate(loanInfo.duration, loanInfo);
+    
+
+
 }
