db8a7a766106734bb206e1802f2458bb6fd64e31
Matt <matthewtmuir@gmail.com>
2021-07-27 22:29:42 -0700

    Working On Math Logic

--- js/site.js
+++ js/site.js
@@ -1,3 +1,44 @@
-function GenerateTableData() {
-    console.log("Hello from JS")
+function GetFormValues() {
+    let principalAmount = document.getElementById("loanAmount").value;
+    let interestRate = document.getElementById("rate").value;
+    let loanLength = document.getElementById("loanTerm").value;
+    let paymentAmount = Number.parseFloat((principalAmount) * (interestRate / 1200) / (1 - (1 + interestRate / 1200)**(0 - loanLength))).toFixed(2);
+    return { principalAmount, interestRate, loanLength, paymentAmount};
+}
+
+function GenerateTableTemplate(loanLength) {
+    const rowTemplate = "<tr><td>?</td><td>?</td><td>?</td><td>?</td><td>?</td><td>?</td></tr>"
+    let tableTemplate = "";
+
+    for (let index = 0; index < loanLength; index++) {
+        tableTemplate += rowTemplate;
+    }
+    return tableTemplate;
+}
+
+
+
+function InputDataIntoTableTemplate() {
+    let formValues = GetFormValues();
+    let tableTemplate = GenerateTableTemplate(formValues.loanLength);
+
+    for (let index = 1; index <= formValues.loanLength; index++) {
+
+        
+        tableTemplate = tableTemplate.replace("?", index.toString());
+        tableTemplate = tableTemplate.replace("?", formValues.paymentAmount.toString());
+        tableTemplate = tableTemplate.replace("?", );
+        tableTemplate = tableTemplate.replace("?", );
+        tableTemplate = tableTemplate.replace("?", );
+        tableTemplate = tableTemplate.replace("?", (formValues.principalAmount - (formValues.paymentAmount * index)).toString() );
+        
+    }
+
+    return tableTemplate;
+}
+
+
+
+function BtnClickEventFunction() {
+    document.getElementById("results").innerHTML = InputDataIntoTableTemplate();
 }
