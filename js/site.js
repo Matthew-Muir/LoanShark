9164c83457245ebdbb3987280a4e498f9a7f3bb2
MattWellDressed <mattm@lcpud.org>
2021-07-28 15:01:11 -0700

    First Draft

--- js/site.js
+++ js/site.js
@@ -22,6 +22,14 @@ function InputDataIntoTableTemplate(loanLength ,loanObj) {
         tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[4]).toFixed(2));
         tableTemplate = tableTemplate.replace("?", Number.parseFloat(holder[5]).toFixed(2));
         
+        if (index == theLoan.duration) {
+            document.getElementById("monthlyPayment").innerHTML = "$" + Number.parseFloat(theLoan.totalPayment).toFixed(2);
+            document.getElementById("totalPrincipal").innerHTML = "$" + Number.parseFloat(theLoan.amount).toFixed(2);
+            document.getElementById("totalInterest").innerHTML = "$" + Number.parseFloat(holder[4]).toFixed(2);
+            document.getElementById("totalCost").innerHTML = "$" + Number.parseFloat(theLoan.amount + holder[4]).toFixed(2);
+
+        }
+        
     }
 
     return tableTemplate;
@@ -30,41 +38,47 @@ function InputDataIntoTableTemplate(loanLength ,loanObj) {
 
 
 function BtnClickEventFunction() {
-    const loanInfo = {
-        amount: parseFloat(document.getElementById("loanAmount").value),
-        balance: parseFloat(document.getElementById("loanAmount").value),
-        paymentsLeft: parseInt(document.getElementById("loanTerm").value),
-        interest: parseFloat(document.getElementById("rate").value),
-        duration: parseInt(document.getElementById("loanTerm").value),
-        get totalPayment(){
-            return (this.amount) * (this.interest / 1200) / (1 - (1 + this.interest / 1200)**(0 - this.duration));
-        },
-        interestPaid: 0,
-        interestPayment: 0,
-        principalPayment: 0,
-        loanUpdate: function(){
-            if (this.amount == this.balance) {
-                this.interestPayment = this.amount * (this.interest / 1200);
-                this.interestPaid += this.interestPayment;
-                this.principalPayment = this.totalPayment - this.interestPayment;
-                this.balance = this.amount - this.principalPayment;
-                //this.paymentsLeft--;
-                
+    if (parseFloat(document.getElementById("loanAmount").value) && parseInt(document.getElementById("loanTerm").value) && parseFloat(document.getElementById("rate").value)) {
+        const loanInfo = {
+            amount: parseFloat(document.getElementById("loanAmount").value),
+            balance: parseFloat(document.getElementById("loanAmount").value),
+            paymentsLeft: parseInt(document.getElementById("loanTerm").value),
+            interest: parseFloat(document.getElementById("rate").value),
+            duration: parseInt(document.getElementById("loanTerm").value),
+            get totalPayment(){
+                return (this.amount) * (this.interest / 1200) / (1 - (1 + this.interest / 1200)**(0 - this.duration));
+            },
+            interestPaid: 0,
+            interestPayment: 0,
+            principalPayment: 0,
+            loanUpdate: function(){
+                if (this.amount == this.balance) {
+                    this.interestPayment = this.amount * (this.interest / 1200);
+                    this.interestPaid += this.interestPayment;
+                    this.principalPayment = this.totalPayment - this.interestPayment;
+                    this.balance = this.amount - this.principalPayment;
+                    //this.paymentsLeft--;
+                    
+                }
+                else
+                {
+                    this.interestPayment = this.balance * (this.interest / 1200);
+                    this.interestPaid += this.interestPayment;
+                    this.principalPayment = this.totalPayment - this.interestPayment;
+                    this.balance = this.balance - this.principalPayment;
+                    //this.paymentsLeft--;
+                }
+        
+                return [this.paymentsLeft--, this.totalPayment, this.principalPayment, this.interestPayment, this.interestPaid, this.balance]
             }
-            else
-            {
-                this.interestPayment = this.balance * (this.interest / 1200);
-                this.interestPaid += this.interestPayment;
-                this.principalPayment = this.totalPayment - this.interestPayment;
-                this.balance = this.balance - this.principalPayment;
-                //this.paymentsLeft--;
-            }
-    
-            return [this.paymentsLeft--, this.totalPayment, this.principalPayment, this.interestPayment, this.interestPaid, this.balance]
-        }
-    };
-    
-    document.getElementById("results").innerHTML = InputDataIntoTableTemplate(loanInfo.duration, loanInfo);
+        };
+        
+        document.getElementById("results").innerHTML = InputDataIntoTableTemplate(loanInfo.duration, loanInfo);
+        
+    }
+    else {
+        alert("All Fields Must Contain a Value Greater Than Zero.");
+    }
     
 
 
